b6ebc1b3e84c00d8255d5bccf937562375783ce7
SakilMostak <skmahim71@gmail.com>
2022-09-03 16:20:12 +0530

    made the server

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("hello");
+const express = require('express');
+const path = require('path');
+const bodyParser= require('body-parser'); //depracated middleware module
+const port= 3000;
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(express.urlencoded());
+
+app.use('/',require('./routes'));
+
+app.listen(port,function(err){
+    //to find error
+    if(err){
+        console.log("Error in running the server",err);
+    }
+
+    console.log('Express server is running smoooooth on Port:',port);
+});
